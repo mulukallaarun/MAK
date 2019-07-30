4a3b1eaafa5da929d24e66197c4d458ca06a84d3
Arun Mulukalla <amulukalla@webmd.net>
2019-07-30 19:38:28 +0530

    testing hook 5

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo 'hello 2';
+prrrr 'hh';
 print phpinfo();
 
+return 0;
 ?>
---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+
+testing();
+return FALSE;
+/**
+ERROR: Parse errors
+INFO: Sensor PHP sensor [php]
+INFO: 2 source files to be analyzed
+ERROR: Unable to parse file [file:///D:/projects/VLabs/SonarQube/SimpleTest/MAK/index.php] at line 3
+ERROR: Parse error at line 3 column 7:
+
+1: <?php
+2: echo 'hello 2';
+3: prrrr 'hh';
+         ^
+4: print phpinfo();
+5:
+6: return 0;
+7: ?>
+8:
+
+INFO: 2/2 source files have been analyzed
+INFO: No PHPUnit test report provided (see 'sonar.php.tests.reportPath' property)
+*/
+?>
